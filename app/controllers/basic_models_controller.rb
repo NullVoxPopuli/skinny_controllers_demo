fe6ec44829605f81e081e56e519f32d297448178
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-17 20:09:04 -0400

    diff of old vs new -- default vs skinny_controllers
    remove ap

--- app/controllers/basic_models_controller.rb
+++ app/controllers/basic_models_controller.rb
@@ -1,51 +1,35 @@
 class BasicModelsController < ApplicationController
-  before_action :set_basic_model, only: [:show, :update, :destroy]
+  include SkinnyControllers::Diet
 
-  # GET /basic_models
   def index
-    @basic_models = BasicModel.all
-
-    render json: @basic_models
+    render json: model
   end
 
-  # GET /basic_models/1
   def show
-    render json: @basic_model
+    render json: model
   end
 
-  # POST /basic_models
   def create
-    @basic_model = BasicModel.new(basic_model_params)
-
-    if @basic_model.save
-      render json: @basic_model, status: :created, location: @basic_model
-    else
-      render json: @basic_model.errors, status: :unprocessable_entity
-    end
+    render json: model, status: 201
   end
 
-  # PATCH/PUT /basic_models/1
   def update
-    if @basic_model.update(basic_model_params)
-      render json: @basic_model
-    else
-      render json: @basic_model.errors, status: :unprocessable_entity
-    end
+    render json: model
   end
 
-  # DELETE /basic_models/1
   def destroy
-    @basic_model.destroy
+    # normally, we'd do this, but the existing tests require a 204
+    # render json: model
+    # this technically violates what 204 in intended for
+    render json: model, status: 204
+    # an alternative could be to either not user 204, or to call model
+    # before the call to render
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_basic_model
-      @basic_model = BasicModel.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
-    def basic_model_params
+    def update_basic_model_params
       params.require(:basic_model).permit(:name, :description)
     end
 end
